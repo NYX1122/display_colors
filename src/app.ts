6c3412a43b61a5eee591b57cade379eefd2e1265
BoDee Angus <bodee.angus@gmail.com>
2024-03-30 15:48:47 -0600

    bla

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import chalk from 'chalk';
 
 const displayColors = (colors: Array<string>): void => {
   for (const color of colors) {
-    console.log(chalk.hex(color)({ color }));
+    const coloredChalk = chalk.hex(color);
+    console.log(coloredChalk(color));
   }
 };
 
